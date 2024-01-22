23acfbb1d0de4a5093c1544ec66a3b3f5a63adea
lolito-lindsey-official <lolitolindsey@gmail.com>
2024-01-22 11:51:05 -0600

    Fetched data from advice slip API
    I added async functions to: fetch and output the advice, update the text content from the HTML, fetch and output the slip object, and loaded the generated advice (upon loading and clicking the dice button).

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-// Add an event listener to fetch data from the Advice Slip JSON API
+const apiLink = "https://api.adviceslip.com/advice";
+// Grabs the advice title, content, and button
+const adviceNumberId = document.querySelector("#advice-title");
+const adviceDisplayContent = document.querySelector("#advice-content");
+const fetchAdviceButton = document.querySelector("#generate-button");
 
-// Loads a slip object, a simple piece of advice
-
-window.addEventListener("DOMContentLoaded", FetchData)
-
-// Uses an asynchronus function
-async function FetchData () {
-    // Awaits fetched data from the API
-    const apiResponse = await fetch("https://api.adviceslip.com/advice");
+// fetches data from the Advice Slip JSON API
+// Uses an async function to fetch data while other tasks are being completed
+const fetchUserAdvice = async () => {
+     // Awaits fetched data from the API
+    const recieveResponse = await fetch(apiLink);
     // Awaits a loaded JSON file containing the slip object data and logs it
-    const jsonForm = await apiResponse.json();
-
-    console.log(jsonForm);
+    const adviceOut = await recieveResponse.json();
+    // Returns the fetched data
+    return adviceOut;
 }
+
+// Updates the advice title and content to the browser (render)
+const updateTitledContent = (fetchedObj) => {
+    // Destructures the object into smaller parts
+    const {id, advice} = fetchedObj;
+
+    adviceNumberId.textContent = `Advice ${id}`;
+    adviceDisplayContent.textContent = advice;
+}
+
+// Takes the fetched data (siip) and passes it to the updateTitledContent function
+const generateNewAdvice = async () => {
+    const genData = await fetchUserAdvice();
+    const adviceOut = genData.slip;
+
+    updateTitledContent(adviceOut);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    // When the generate button is clicked, it will generate new advice
+    fetchAdviceButton.addEventListener('click', generateNewAdvice);
+    // Fetches advice upon the page loading
+    generateNewAdvice();
+});
+
